2bfa2fae2524283bb540924a440c0cf2877a4a14
AbigailRV <abigail.raymundo.v@gmail.com>
2026-04-23 17:29:29 -0500

    feat: seccion de testimonios completada con diseño de ondas y responsive

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import {LanguageService} from '../../core/services/lenguage.service';
 
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.html',
   styleUrl: './about.css',
+  standalone: true, // Asumo que usas Standalone por los errores anteriores
 })
 export class About {
-
+  constructor(public langService: LanguageService) {}
 }
--- src/app/components/testimonials/testimonials.ts
+++ src/app/components/testimonials/testimonials.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import {LanguageService} from '../../core/services/lenguage.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-testimonials',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './testimonials.html',
-  styleUrl: './testimonials.css',
+  styleUrls: ['./testimonials.css']
 })
 export class Testimonials {
+  constructor(public langService: LanguageService) {}
 
+  testimonios = [
+    {
+      nombre: 'Carlos Ramirez',
+      lugar: 'Lima, Perú',
+      texto: 'testimonioCarlos',
+      foto: './assets/carlos.jpg'
+    },
+    {
+      nombre: 'Andrea Mendoza',
+      lugar: 'Lima, Perú',
+      texto: 'testimonioAndrea',
+      foto: './assets/andrea.jpg'
+    },
+    {
+      nombre: 'Andres Vela',
+      lugar: 'Lima, Perú',
+      texto: 'testimonioAndres',
+      foto: './assets/andres.png'
+    }
+  ];
 }
