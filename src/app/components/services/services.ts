e4faec0224cc0568449160b2ea86379c07fe4c94
AbigailRV <abigail.raymundo.v@gmail.com>
2026-04-22 23:20:11 -0500

    feat: carousel de servicios

--- src/app/components/services/services.ts
+++ src/app/components/services/services.ts
@@ -8,6 +8,63 @@ import {LanguageService} from '../../core/services/lenguage.service';
   styleUrl: './services.css',
 })
 export class Services {
+  servicios = [
+    {
+      titulo: 'diagnosticoAvanzado', // Usa la llave de traducción
+      imagen: './assets/diagnostico-avanzado.png'
+    },
+    {
+      titulo: 'unidadNutricion',
+      imagen: './assets/nutricion.png'
+    },
+    {
+      titulo: 'farmacovigilancia',
+      imagen: './assets/farmacovigilancia.png'
+    },
+    {
+      titulo: 'educacionDiabetologica',
+      imagen: './assets/educacion-diabetes.png'
+    },
+    {
+      titulo: 'investigacionTerapias',
+      imagen: './assets/terapias.png'
+    },
+    {
+      titulo: 'farmaciaEspecializada',
+      imagen: './assets/farmacia.png'
+    },
+    {
+      titulo: 'rehabilitacionFisica',
+      imagen: './assets/rehabilitacion.png'
+    },
+    {
+      titulo: 'medicinaIntegrativa',
+      imagen: './assets/medicina-integrativa.png'
+    },
+    {
+      titulo: 'historialMedico',
+      imagen: './assets/historial-medico.png'
+    },
+    {
+      titulo: 'convertidorPdf',
+      imagen: './assets/pdf.png'
+    },
+    {
+      titulo: 'controlDatos',
+      imagen: './assets/control-datos.png'
+    },
+  ];
+
+  indiceActual = 0;
   constructor(public langService: LanguageService) {}
+
+  // flechas
+  siguiente() {
+    this.indiceActual = (this.indiceActual + 1) % this.servicios.length;
+  }
+
+  anterior() {
+    this.indiceActual = (this.indiceActual - 1 + this.servicios.length) % this.servicios.length;
+  }
 
 }
--- src/app/pages/pages/pages.ts
+++ src/app/pages/pages/pages.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {Contact} from "../../components/contact/contact";
 import {Hero} from "../../components/hero/hero";
 import {Navbar} from "../../shared/navbar/navbar";
+import { Services } from '../../components/services/services';
 
 @Component({
   selector: 'app-pages',
     imports: [
         Contact,
         Hero,
-        Navbar
+        Navbar,
+      Services
     ],
   templateUrl: './pages.html',
   styleUrl: './pages.css',
